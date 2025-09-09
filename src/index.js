b8624d1956228afbcb13af11b911c1c1016ddf9d
Tran Van Luong <tranvanluong032020@gmail.com>
2025-09-09 23:37:31 +0700

    done

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path"
 import { app, server } from "./lib/socket.js"
 
 const port = process.env.PORT
 const frontend_url = process.env.REACT_URL
+const __dirname = path.resolve()
 
 dotenv.config()
 app.use(express.json({ limit: "10mb" }))
@@ -20,6 +22,14 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(port, () => {
     console.log("server is running on port: " + port)
